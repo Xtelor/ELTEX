2a216333761af1a7b538c7d2501f90f9d5d4d0d2
Xtelor <68063091+Xtelor@users.noreply.github.com>
2020-07-30 22:43:58 +0700

    Update multiplication.c

--- Calculator/multiplication.c
+++ Calculator/multiplication.c
@@ -1,13 +1,12 @@
 #include "header.h"
 
     float multiplication(float num1, float num2)
-		{
+    {
+        printf("Input first multiplier: \n");
+	scanf("%f", &num1);
 
-	      printf("Input first multiplier: \n");
-				scanf("%f", &num1);
+	printf("Input second multiplier: \n");
+	scanf("%f", &num2);
 
-				printf("Input second multiplier: \n");
-				scanf("%f", &num2);
-
-    return (num1 * num2);
+    	return (num1 * num2);
     }
