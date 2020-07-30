e5ef3f57d6440893f6218b95462d76a9239b8964
Xtelor <68063091+Xtelor@users.noreply.github.com>
2020-07-30 22:46:07 +0700

    Update sum.c

--- Calculator/sum.c
+++ Calculator/sum.c
@@ -2,12 +2,11 @@
 
     float sum(float num1, float num2)
     {
+        printf("Input first term: \n");
+	scanf("%f", &num1);
 
-	    printf("Input first term: \n");
-	    scanf("%f", &num1);
+	printf("Input second term: \n");
+	scanf("%f", &num2);
 
-	    printf("Input second term: \n");
-	    scanf("%f", &num2);
-
-    return (num1 + num2);
+        return (num1 + num2);
     }
